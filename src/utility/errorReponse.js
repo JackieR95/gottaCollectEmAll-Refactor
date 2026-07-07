25e4140695dbc12109a1f8c2477755d1dddd2fcc
Jacqueline <156032975+JackieR95@users.noreply.github.com>
2026-07-06 18:40:51 -0700

    error repsonse page with commented code, will change later

---
+++ src/utility/errorReponse.js
@@ -0,0 +1,12 @@
+// class ErrorResponse {
+//   status;
+
+//   constructor(status) {
+//     this.status = status;
+//   }
+
+//   getMessage() {
+//     throw new Error(`HTTP error! Status: ${this.status}`); // Throw an error if the response is not ok
+//     return;
+//   }
+// }
